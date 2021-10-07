37592db33a4de27c4f6dae9e94f470f364537469
Xu Li <xu.li@milliporesigma.com>
2021-10-07 14:52:55 -0400

    Added back VI analysis

--- Nitride.EE/Source/DataTable/ParamTable/ZParam.cs
+++ Nitride.EE/Source/DataTable/ParamTable/ZParam.cs
@@ -46,28 +46,61 @@ namespace Nitride.EE
 
         public void GetZ1(FreqTable ft, ComplexColumn z2Column, ComplexColumn z1Column)
         {
-            //var freq_list = z2_list.Keys.OrderBy(n => n);
-
-            //ZParamTable zt = new ZParamTable(freq_list, 2);
-
-            //Interpolate(zt);
-            //Dictionary<double, Complex> result = new();
-
             foreach (FreqRow row in ft.Rows)
             {
-
-                //double freq = row.Frequency;
-
-                Complex z11 = row[this[1, 1]]; // = new Complex(result_s11_real[pt], result_s11_imag[pt]);
-                Complex z12 = row[this[1, 2]]; // = new Complex(result_s12_real[pt], result_s12_imag[pt]);
-                Complex z21 = row[this[2, 1]]; // = new Complex(result_s21_real[pt], result_s21_imag[pt]);
-                Complex z22 = row[this[2, 2]]; // = new Complex(result_s22_real[pt], result_s22_imag[pt]);
+                Complex z11 = row[this[1, 1]];
+                Complex z12 = row[this[1, 2]];
+                Complex z21 = row[this[2, 1]];
+                Complex z22 = row[this[2, 2]];
                 Complex z2 = row[z2Column];
-
                 row[z1Column] = z11 - (z12 * z21 / (z22 + z2));
             }
+        }
 
-            //return result;
+        // Get Z2
+
+        public void CalculatePort2VI(FreqTable ft)
+        {
+            var (V1_Column, I1_Column, P1_Column) = this[1];
+            var (V2_Column, I2_Column, P2_Column) = this[2];
+
+            foreach (var row in ft.Rows)
+            {
+                Complex z11 = row[this[1, 1]];
+                Complex z12 = row[this[1, 2]];
+                Complex z21 = row[this[2, 1]];
+                Complex z22 = row[this[2, 2]];
+                
+                Complex v1 = row[V1_Column];
+                Complex i1 = row[I1_Column];
+                row[P1_Column] = v1 * i1;
+
+                Complex i2 = row[I2_Column] = (v1 - (z11 * i1)) / z12;
+                Complex v2 = row[V2_Column] = (z21 * i1) + (z22 * i2);
+                row[P2_Column] = v2 * i2;
+            }
+        }
+
+        public void CalculatePort1VI(FreqTable ft)
+        {
+            var (V1_Column, I1_Column, P1_Column) = this[1];
+            var (V2_Column, I2_Column, P2_Column) = this[2];
+
+            foreach (var row in ft.Rows)
+            {
+                Complex z11 = row[this[1, 1]];
+                Complex z12 = row[this[1, 2]];
+                Complex z21 = row[this[2, 1]];
+                Complex z22 = row[this[2, 2]];
+
+                Complex v2 = row[V2_Column];
+                Complex i2 = row[I2_Column];
+                row[P2_Column] = v2 * i2;
+
+                Complex i1 = row[I1_Column] = (v2 - (z22 * i2)) / z21;
+                Complex v1 = row[V1_Column] = (z11 * i1) + (z12 * i2);
+                row[P1_Column] = v1 * i1;
+            }
         }
     }
 }
