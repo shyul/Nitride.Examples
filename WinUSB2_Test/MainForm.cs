7fef275f81e095747a74b6a9e47ddb26487a7c1a
shyul <me@xuli.us>
2021-10-28 12:51:09 -0400

    Merge FFT with WinUSB

--- WinUSB2_Test/MainForm.cs
+++ WinUSB2_Test/MainForm.cs
@@ -10,11 +10,31 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Nitride;
 using Nitride.EE.WinUSB;
+using Nitride.Chart;
+using Nitride.EE;
 
 namespace WindowsFormsApp1
 {
     public partial class MainForm : Form
     {
+        public static NumericColumn Column_Channel1 { get; } = new("Channel 1", "FS");
+
+        public static NumericColumn Column_Channel2 { get; } = new("Channel 2", "FS");
+
+        public ChronoTable SampleTable { get; set; }
+
+        public ChronoChart SampleChart { get; set; }
+
+        FreqTable FreqTable { get; set; }
+
+        FreqChart FreqChart { get; set; }
+
+        FFT FFT { get; }
+
+        const int SampleLength = 65536 * 4;
+
+        public static ComplexColumn Column_ChannelComplex { get; } = new("Complex Channel", "FS");
+
         WinUsbDevice UsbDevice { get; set; } = null;
 
         private Random Random { get; } = new Random();
@@ -38,6 +58,102 @@ namespace WindowsFormsApp1
             }
 
             InitializeComponent();
+
+            var samples = FFT.GetSineWave(SampleLength, 8191, 0.86);
+            /*
+            foreach(var c in samples) 
+            {
+                Console.WriteLine(c.ToString());
+            }*/
+
+            ChronoTable ct = new(samples.Length) { SampleRate = 100e6 };
+
+            for (int i = 0; i < samples.Length; i++)
+            {
+                ct[i][Column_Channel1] = samples[i].Real;
+                ct[i][Column_Channel2] = samples[i].Imaginary;
+                ct[i][Column_ChannelComplex] = samples[i];
+            }
+
+            SampleTable = ct;
+
+            SampleChart = new ChronoChart("Sample Chart", SampleTable)
+            {
+                IndexCount = 500,
+                ReadyToShow = true,
+                StopPt = 1500
+            };
+
+            //SampleChart.MainArea.AxisRight.
+
+            SampleChart.MainArea.AddSeries(new LineSeries(Column_Channel1)
+            {
+                Order = 0,
+                Importance = Importance.Minor,
+                Name = "Channel 1",
+                LegendName = "Channel 1",
+                Color = Color.SlateBlue,
+                IsAntialiasing = true,
+                Tension = 0,
+                HasTailTag = false
+            });
+
+            SampleChart.MainArea.AddSeries(new LineSeries(Column_Channel2)
+            {
+                Order = 0,
+                Importance = Importance.Minor,
+                Name = "Channel 2",
+                LegendName = "Channel 2",
+                Color = Color.DarkGreen,
+                IsAntialiasing = true,
+                Tension = 0,
+                HasTailTag = false
+            });
+
+            SampleChart.Location = new Point(0, 0);
+            SampleChart.Dock = DockStyle.Fill;
+            Program.ChartForm.AddForm(SampleChart);
+
+            FFT = new(SampleLength);// 65536, WindowsType.BlackmanHarris);
+            FreqTable = new FreqTable();
+
+            FreqTable.Configure(0, SampleTable.SampleRate, SampleLength / 64);
+
+            FFT.Transform(FreqTable, SampleTable, Column_ChannelComplex, 0);
+
+            double peak = FreqTable.Rows.Select(n => n[FFT.Column_ResultDb]).Max();
+
+            Console.WriteLine("Peak = " + peak);
+
+            FreqChart = new("Freq Chart", FreqTable)
+            {
+                IndexCount = FreqTable.Count,
+                ReadyToShow = true,
+                StopPt = FreqTable.Count
+            };
+
+            FreqChart.MainArea.AddSeries(new LineSeries(FFT.Column_ResultDb)
+            {
+                Order = 0,
+                Importance = Importance.Major,
+                Name = "FFT Spectrum",
+                LegendName = "FFT Spectrum",
+                Color = Color.Gray,
+                IsAntialiasing = true,
+                Tension = 0,
+                HasTailTag = false
+            });
+
+            FreqChart.Location = new Point(0, 0);
+            FreqChart.Dock = DockStyle.Fill;
+            Program.ChartForm.AddForm(FreqChart);
+
+
+
+
+
+
+
             Guid guid = Guid.Parse("{6AE77B78-518B-493D-A9DD-3A64E87EA3F9}");
             Console.WriteLine("GUID is: " + guid.ToString().ToUpper());
             var pathList = WinUsbDevice.FindDevicePathList(guid);
--- WinUSB2_Test/Program.cs
+++ WinUSB2_Test/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Nitride;
+using Nitride.WindowsNativeMethods;
 
 namespace WindowsFormsApp1
 {
@@ -14,9 +16,35 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            ChartForm.Show();
             Application.Run(new MainForm());
         }
+
+        public static ChartForm ChartForm { get; } = new();
+
+        public static readonly int SHOW_FORM = User32.RegisterWindowMessage("SHOW_NITRIDE_FORM");
+    }
+
+    internal sealed partial class ChartForm : MosaicForm
+    {
+        #region Ctor
+        public ChartForm() : base(Program.SHOW_FORM)
+        {
+            SuspendLayout();
+
+            HelpLink = "https://github.com/shyul/Pacmio/wiki";
+            //Icon = Pacmio.Properties.Resources.Pacman;
+            IsRibbonShrink = true;
+            Text = "Spectrum";
+            Width = 1500;
+            Height = 1200;
+            ResumeLayout(false); // false
+            PerformLayout();
+        }
+
+        #endregion
     }
 }
